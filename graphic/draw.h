c3b43f0ae43b910fbb2ae73894d6229e07534bdf
Tales Bontempo <tabocu@gmail.com>
2017-02-16 17:32:17 -0200

    Created draw and transform headers in graphic

---
+++ graphic/draw.h
@@ -0,0 +1,9 @@
+#ifndef _DRAW_H_
+#define _DRAW_H_
+
+namespace graphic
+{
+
+} 
+
+#endif //_DRAW_H_
---
+++ graphic/transform.h
@@ -0,0 +1,9 @@
+#ifndef _TRANSFORM_H_
+#define _TRANSFORM_H_
+
+namespace graphic
+{
+
+}
+
+#endif //_TRANSFORM_H_
