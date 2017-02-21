e507540ff16119a35a4250efd85308145b2a5e6b
Tales <tabocu@gmail.com>
2017-02-20 21:27:47 -0300

    Finished the basic structure of paint from graphic

--- graphic/paint.cpp
+++ graphic/paint.cpp
@@ -1,1 +1,22 @@
 #include "paint.h"
+
+using namespace graphic;
+
+void paint::draw_line_DDA(char * buffer,
+                          std::pair<uint,uint> size,
+                          std::pair<uint,uint> p1,
+                          std::pair<uint,uint> p2)
+{}
+
+void paint::draw_line_bresenham(char * buffer,
+                                std::pair<uint,uint> size,
+                                std::pair<uint,uint> p1,
+                                std::pair<uint,uint> p2)
+{}
+
+void paint::draw_circle_bresenham(char * buffer,
+                                  std::pair<uint,uint> size,
+                                  std::pair<uint,uint> center,
+                                  std::pair<uint,uint> radius)
+{}
+
--- graphic/paint.h
+++ graphic/paint.h
@@ -1,9 +1,30 @@
 #ifndef _PAINT_H_
 #define _PAINT_H_
 
+#include <utility>
+#include <defines.h>
+#include <matrix.h>
+
 namespace graphic
 {
+    class paint
+    {
+    public:
+        void static draw_line_DDA(char * buffer,
+                                  std::pair<uint,uint> size,
+                                  std::pair<uint,uint> p1,
+                                  std::pair<uint,uint> p2);
 
+        void static draw_line_bresenham(char * buffer,
+                                        std::pair<uint,uint> size,
+                                        std::pair<uint,uint> p1,
+                                        std::pair<uint,uint> p2);
+
+        void static draw_circle_bresenham(char * buffer,
+                                          std::pair<uint,uint> size,
+                                          std::pair<uint,uint> center,
+                                          std::pair<uint,uint> radius);
+    };
 }
 
 #endif //_PAINT_H_
