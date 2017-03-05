301cd9440137d0fa15a700912c219ebd289fcd55
Tales <tabocu@gmail.com>
2017-03-05 16:25:52 -0300

    Added info to the application

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QCoreApplication::setOrganizationName("Tales Bontempo Cunha");
+    QCoreApplication::setApplicationName("Black Vector");
     MainWindow w;
     w.show();
 
