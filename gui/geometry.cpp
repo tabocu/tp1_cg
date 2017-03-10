3ae5a8e122f8c0251f78198c78aa26a6e635c201
Tales <tabocu@gmail.com>
2017-03-10 20:35:48 -0300

    Implemented holdplaces methods for draw in geometry

--- gui/geometry.cpp
+++ gui/geometry.cpp
@@ -7,7 +7,8 @@ graphic::line::line(QPointF p1, QPointF p2)
 
 void graphic::line::paint(QImage &image, QPointF minEdge, QPointF maxEdge) const
 {
-
+    QPainter painter(image);
+    painter.drawLine(m_p1,m_p2);
 }
 
 graphic::circle::circle(QPointF p1, QPointF)
@@ -15,8 +16,9 @@ graphic::circle::circle(QPointF p1, QPointF)
     , m_radius(0)
 {}
 
-void graphic::circle::paint(QImage &, QPointF, QPointF) const
+void graphic::circle::paint(QImage &image, QPointF minEdge, QPointF maxEdge) const
 {
-
+    QPainter painter(image);
+    painter.drawEllipse(m_center,m_radius,m_radius);
 }
 
